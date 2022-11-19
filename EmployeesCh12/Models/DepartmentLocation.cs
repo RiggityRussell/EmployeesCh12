4159f7aa801956d02ebb4d5d15418ff428cd8c27
RiggityRussell <russellgtarlt@gmail.com>
2022-11-19 18:09:40 -0500

    Add project files.

---
+++ EmployeesCh12/Models/DepartmentLocation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesCh12.Models
+{
+    public class DepartmentLocation
+    {
+
+        //Composite Primary key and foreign key for Department
+        public int DepartmentID { get; set; }
+        // COmposite Primary key foreign key for Location
+        public int LocationID { get; set; }
+
+        //Navigation Properties
+        public Department Department { get; set; }
+        public Location Location { get; set; }
+    }
+}
